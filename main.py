4288cd7b24075672041a92e73b8d9d1e30f7b57e
sayaksen <sayak.sen@exotel.in>
2020-05-07 00:22:44 +0530

    init deploy

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, Pratilipi!"
+    return "<h1><center>Hello, Pratilipi!</center></h1>"
     
 if __name__ == "__main__":
     app.run(debug=True)
